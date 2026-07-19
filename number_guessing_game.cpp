4ca8aff8d529f78b4574fe8207a56ac17a2ce91f
Kartik Manik <sweetruby4uonly@gmail.com>
2026-07-20 00:10:33 +0530

    ..

--- number_guessing_game.cpp
+++ number_guessing_game.cpp
@@ -1,4 +1,14 @@
-
+/*******************************************************************************
+*
+* Program: Number Guessing Game
+* 
+* Description: Example of a number guessing game in C++.
+*
+* YouTube Lesson: https://www.youtube.com/watch?v=HTv-TgYaXI8 
+*
+* Author: Kevin Browne @ https://portfoliocourses.com
+*
+*******************************************************************************/
 
 #include <iostream>
 #include <cstdlib>
